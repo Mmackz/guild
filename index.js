361d1f705456e936e4e0855214c040ebe0f5da60
Mmackz <matthewmackenz@gmail.com>
2023-12-25 23:00:00 -0800

    add ticket role update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import cron from "node-cron";
 import { updateRole } from "./lib/updateRole.js";
 
-cron.schedule("0 */6 * * *", updateRole);
+// cron.schedule("0 */6 * * *", updateRole);
+updateRole()
